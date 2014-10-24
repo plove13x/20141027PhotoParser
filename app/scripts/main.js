8e68112d954403e2845dc822913f9b2cdebead80
Patrick Love <plove13x@gmail.com>
2014-10-24 01:15:23 -0400

    Implements login functionality from Parse docs but yet to transition to posts page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,11 +10,19 @@ App.Routers = {};
 
 // MODELS
 
+App.Post = Parse.Object.extend({
+  className: 'post'
+});
+
+App.Comment = Parse.Object.extend({
+  className: 'comment'
+});
+
 
 // VIEWS
 
 App.LogInView = Parse.View.extend({
-	template: _.template($('.login-template').text()),
+	template: _.template($('[data-template-name="login"]').text()),
 
 	events: {
 		'submit': 'userLogIn',
@@ -22,14 +30,30 @@ App.LogInView = Parse.View.extend({
 
 	userLogIn: function(e){
 	    e.preventDefault();
+	    Parse.User.logOut();							/* Test to see if necessary */
 	    var email = this.$('.email-input').val();
 	    var password = this.$('.password-input').val();
-	    Parse.User.logIn(email, password).then(function(user){
-	    	console.log(user);
-	      // PPRouter.navigate('/posts/create', {trigger: true});
+	    var username = this.$('.username-input').val();
+	 
+
+Parse.User.logIn(username, password, {
+  success: function(user) {
+    // Do stuff after successful login.
+    console.log('y');
+  },
+  error: function(user, error) {
+    // The login failed. Check error to see why.
+    console.log('n');
+  }
+});
+
+	    // Parse.User.logIn(email, password).then(function(user){
+	    // 	console.log(user);
+	    //   PPRouter.navigate('/posts/create', {trigger: true});
 	    // }, function(error){
 	    //   console.error(error);
-	    });
+	    // });
+
 	  },
 
 	initialize: function(){
@@ -45,7 +69,7 @@ App.LogInView = Parse.View.extend({
 
 App.RegisterView = Parse.View.extend({
 
-	template: _.template($('.register-template').text()),
+	template: _.template($('[data-template-name="register"]').text()),
 
 	events: {
 		'submit': 'userRegister',
@@ -59,9 +83,9 @@ App.RegisterView = Parse.View.extend({
 		var username = this.$('.username-input').val();
 
 		var user = new Parse.User();
-		user.set("username", username);
-		user.set("password", password);
-		user.set("email", email);
+		user.set('username', username);
+		user.set('password', password);
+		user.set('email', email);
 		 
 		user.signUp(null, {
 		  success: function(user) {
@@ -85,6 +109,21 @@ App.RegisterView = Parse.View.extend({
 });
 
 
+App.PostsIndexView = Parse.View.extend({
+
+	template: _.template( $('[data-template-name="posts/index"]').text() ),
+  
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html(this.template);
+		return this;
+	}
+
+});
+
 
 // ROUTER(S)
 
@@ -93,10 +132,11 @@ App.AppRouter = Parse.Router.extend({
 		//URL to match	//function called when the hash matches
 
 		''				: 'index',				//	url/#
-		'login'			: 'login',			//	url/#aboutme
+		'login'			: 'login',				//	url/#login
+		'register'		: 'register',			//	url/#register
+		'posts'			: 'postsIndex',			//  url/#posts
 		'setlang'		: 'renderSetLanguages',	//	url/#setlang
-		'register'		: 'register',				//	url/#tomeet
-		'messenger'		: 'renderMessenger'			//	url/#messenger
+		'messenger'		: 'renderMessenger'		//	url/#messenger
 	},
 
 	index: function(){
@@ -108,12 +148,24 @@ App.AppRouter = Parse.Router.extend({
 	},
   
 	login: function(){
-		new App.LogInView({el: 'body'});
+		this.swap( new App.LogInView({el: 'body'}) );
 	},
 
 	register: function(){
-		new App.RegisterView({el: 'body'});
-	}
+		this.swap( new App.RegisterView({el: 'body'}) );
+	},
+
+	postsIndex: function(){
+		this.swap( new App.PostsIndexView({el: 'body'}) );
+	},
+
+	swap: function(view){
+		if (this.currentView) {
+			this.currentView.remove();
+			this.currentView = view;
+			// this.currentView.render();
+  		}
+  	},
 
 });
 
