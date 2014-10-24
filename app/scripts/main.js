61fdea8f1471c95cdec1c997af201859cc1dc93f
Patrick Love <plove13x@gmail.com>
2014-10-23 23:52:23 -0400

    Adds user registration functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,12 +24,12 @@ App.LogInView = Parse.View.extend({
 	    e.preventDefault();
 	    var email = this.$('.email-input').val();
 	    var password = this.$('.password-input').val();
-	    // Parse.User.logIn(email, password).then(function(user){
-	    // 	console.log(user);
+	    Parse.User.logIn(email, password).then(function(user){
+	    	console.log(user);
 	      // PPRouter.navigate('/posts/create', {trigger: true});
 	    // }, function(error){
 	    //   console.error(error);
-	    // });
+	    });
 	  },
 
 	initialize: function(){
@@ -43,6 +43,49 @@ App.LogInView = Parse.View.extend({
 });
 
 
+App.RegisterView = Parse.View.extend({
+
+	template: _.template($('.register-template').text()),
+
+	events: {
+		'submit': 'userRegister',
+	},
+
+	userRegister: function(e){
+		e.preventDefault();
+
+		var email = this.$('.email-input').val();
+		var password = this.$('.password-input').val();
+		var username = this.$('.username-input').val();
+
+		var user = new Parse.User();
+		user.set("username", username);
+		user.set("password", password);
+		user.set("email", email);
+		 
+		user.signUp(null, {
+		  success: function(user) {
+		    // Hooray! Let them use the app now.
+		  },
+		  error: function(user, error) {
+		    // Show the error message somewhere and let the user try again.
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+		});
+	},
+
+	initialize: function(){
+		this.render();
+	},
+
+	render: function(){
+		this.$el.html(this.template);
+		return this;
+	},
+});
+
+
+
 // ROUTER(S)
 
 App.AppRouter = Parse.Router.extend({
@@ -52,7 +95,7 @@ App.AppRouter = Parse.Router.extend({
 		''				: 'index',				//	url/#
 		'login'			: 'login',			//	url/#aboutme
 		'setlang'		: 'renderSetLanguages',	//	url/#setlang
-		'tomeet'		: 'renderToMeet',				//	url/#tomeet
+		'register'		: 'register',				//	url/#tomeet
 		'messenger'		: 'renderMessenger'			//	url/#messenger
 	},
 
@@ -67,6 +110,10 @@ App.AppRouter = Parse.Router.extend({
 	login: function(){
 		new App.LogInView({el: 'body'});
 	},
+
+	register: function(){
+		new App.RegisterView({el: 'body'});
+	}
 
 });
 
